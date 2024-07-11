83344fae8060f89bbb07327ae1b370160e77d3b0
Diego Araujo <ferreiradearaujod@gmail.com>
2024-07-11 08:51:53 -0300

    feat: conteudo novo metodos de array

--- script-02.js
+++ script-02.js
@@ -1,3 +1,19 @@
-var pessoa = { nome: 'Arthur', idade: 40 }
-console.log(pessoa.nome);
-console.log(pessoa.idade);
+var pessoalA = {
+    nome: 'Arthur',
+    idade: 40,
+    favoritos: ['Filmes', 'Jogos'],
+}
+
+var pessoalB = {
+    nome: 'Mannu',
+    idade: 30,
+    favoritos: ['Pizza', 'Sushui'],
+}
+
+
+
+var listaPessoas = [pessoalA, pessoalB]
+
+console.log(listaPessoas.map((pessoa) => pessoa.nome));
+console.log(listaPessoas.map((pessoa) => pessoa.idade));
+console.log(listaPessoas.map((pessoa) => pessoa.favoritos));
